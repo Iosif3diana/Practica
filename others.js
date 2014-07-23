8ab2c47db425029a38f9afa014e88faef761370e
Iosif3diana <iosif3diana@yahoo.com>
2014-07-23 16:27:51 +0300

    Create others.js

---
+++ others.js
@@ -0,0 +1,18 @@
+/**
+ * The first menu function
+ */
+function addHabbit() {
+alert("in >>");
+window.open("others.html");
+
+/*var table = document.getElementById("tableBody");
+var row = table.insertRow(0);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+cell1.innerHTML = "Habbit Test";
+cell3.innerHTML = "Check ";
+var img = document.createElement('img');
+img.src = "http://icons.iconarchive.com/icons/saki/nuoveXT-2/128/Status-image-loading-icon.png";
+cell2.appendChild(img);*/
+}	
