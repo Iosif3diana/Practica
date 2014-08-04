ae6a73790e63ccf6a483321ad6f0ac439c6b2e6c
Iosif3diana <iosif3diana@yahoo.com>
2014-08-04 16:58:08 +0300

    Update others.js

--- others.js
+++ others.js
@@ -3,16 +3,23 @@
  */
 function addHabbit() {
 alert("in >>");
-window.open("others.html");
+window.open("others.html");}
 
-/*var table = document.getElementById("tableBody");
-var row = table.insertRow(0);
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-cell1.innerHTML = "Habbit Test";
-cell3.innerHTML = "Check ";
-var img = document.createElement('img');
-img.src = "http://icons.iconarchive.com/icons/saki/nuoveXT-2/128/Status-image-loading-icon.png";
-cell2.appendChild(img);*/
-}	
+/**
+ * Date calendar
+ */
+$(document).ready(function(){
+	$('#incomeDate').datepicker();
+});
+
+$('input.incomeDate').datepicker({
+    beforeShow: function(input, inst)
+    {
+        inst.dpDiv.css({marginTop: -input.offsetHeight + 'px', marginLeft: input.offsetWidth + 'px'});
+    }
+});
+
+$("start").onclick(function (){
+	$('#action-menu').style;
+});
+
